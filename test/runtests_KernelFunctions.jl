ffde9ff5e51937ea9bd8c4f7a3e8ddd7e0b0257a
Erik Schnetter <schnetter@gmail.com>
2023-12-21 21:42:39 -0500

    Run Metal and CUDA tests

--- test/runtests_KernelFunctions.jl
+++ test/runtests_KernelFunctions.jl
@@ -31,3 +31,82 @@ Random.seed!(0)
         test_Category(Box{DS,S}, Box{DT,T}, KernelFunction{DS,S,DT,T}, make_dom, make_cod, nothing, make_kf)
     end
 end
+
+@static if VERSION >= v"1.8"
+    using Metal
+    if Metal.functional()
+        Random.seed!(0)
+        @testset "KernelFunction [Metal] S=$S^$DS T=$T^$DT" for DS in 0:4, S in [Float32, Double32], DT in 0:4, T in [S]
+            function make_dom()
+                xmin = random_SVector(
+                    SVector{DS,S}(S(-1) for d in 1:DS), SVector{DS,S}(S(1//100) for d in 1:DS), SVector{DS,S}(S(+1) for d in 1:DS)
+                )
+                xmax = random_SVector(xmin .+ 1, SVector{DS,S}(S(1//100) for d in 1:DS), xmin .+ 10)
+                dom = Box(xmin, xmax)
+                values = [random_SVector(xmin, SVector{DS,S}(S(1//100) for d in 1:DS), xmax) for n in 1:10]
+                return dom, values
+            end
+            function make_cod()
+                ymin = random_SVector(
+                    SVector{DT,T}(T(-1) for d in 1:DT), SVector{DT,T}(T(1//100) for d in 1:DT), SVector{DT,T}(T(+1) for d in 1:DT)
+                )
+                ymax = random_SVector(ymin .+ 1, SVector{DT,T}(T(1//100) for d in 1:DT), ymin .+ 10)
+                cod = Box(ymin, ymax)
+                values = [random_SVector(ymin, SVector{DT,T}(T(1//100) for d in 1:DT), ymax) for n in 1:10]
+                return cod, values
+            end
+            function make_kf(dom, cod)
+                npoints = rand(2:10, DS)
+                grid = [
+                    random_SVector(first(cod), SVector{DT,T}(1//100 for d in 1:DT), last(cod)) for
+                    i in CartesianIndices(Tuple(npoints))
+                ]
+                grid::AbstractArray{SVector{DT,T},DS}
+                grid = MtlArray(grid)
+                kf = KernelFunction("kf", dom, cod, grid)
+                return kf
+            end
+            for iter in 1:10
+                test_Category(Box{DS,S}, Box{DT,T}, KernelFunction{DS,S,DT,T}, make_dom, make_cod, nothing, make_kf)
+            end
+        end
+    end
+end
+
+using CUDA
+if CUA.functional()
+    Random.seed!(0)
+    @testset "KernelFunction [CUDA] S=$S^$DS T=$T^$DT" for DS in 0:4, S in [Float32, Float64, Double64], DT in 0:4, T in [S]
+        function make_dom()
+            xmin = random_SVector(
+                )
+                SVector{DS,S}(S(-1) for d in 1:DS), SVector{DS,S}(S(1//100) for d in 1:DS), SVector{DS,S}(S(+1) for d in 1:DS)
+            xmax = random_SVector(xmin .+ 1, SVector{DS,S}(S(1//100) for d in 1:DS), xmin .+ 10)
+            dom = Box(xmin, xmax)
+            values = [random_SVector(xmin, SVector{DS,S}(S(1//100) for d in 1:DS), xmax) for n in 1:10]
+            return dom, values
+        end
+        function make_cod()
+            ymin = random_SVector(
+                SVector{DT,T}(T(-1) for d in 1:DT), SVector{DT,T}(T(1//100) for d in 1:DT), SVector{DT,T}(T(+1) for d in 1:DT)
+            )
+            ymax = random_SVector(ymin .+ 1, SVector{DT,T}(T(1//100) for d in 1:DT), ymin .+ 10)
+            cod = Box(ymin, ymax)
+            values = [random_SVector(ymin, SVector{DT,T}(T(1//100) for d in 1:DT), ymax) for n in 1:10]
+            return cod, values
+        end
+        function make_kf(dom, cod)
+            npoints = rand(2:10, DS)
+            grid = [
+                random_SVector(first(cod), SVector{DT,T}(1//100 for d in 1:DT), last(cod)) for i in CartesianIndices(Tuple(npoints))
+            ]
+            grid::AbstractArray{SVector{DT,T},DS}
+            grid = CuArray(grid)
+            kf = KernelFunction("kf", dom, cod, grid)
+            return kf
+        end
+        for iter in 1:10
+            test_Category(Box{DS,S}, Box{DT,T}, KernelFunction{DS,S,DT,T}, make_dom, make_cod, nothing, make_kf)
+        end
+    end
+end
